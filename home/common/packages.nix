3fb552a94a78cd4e3b07d0aeb9b77f6c59cefd68
ZakzoukBT <zakaria.bentanfous@gmail.com>
2026-07-18 22:16:35 +0200

    feat & clean: add copyq that behaves like windows paste history

--- home/common/packages.nix
+++ home/common/packages.nix
@@ -7,14 +7,36 @@
     fd
     eza
     bat
-    jq
     tree
     unzip
     zip
     btop
     nvtopPackages.amd
     wl-clipboard
-    pciutils
+    copyq
     net-tools
   ];
+
+  # Autostart CopyQ on login
+  xdg.configFile."autostart/copyq.desktop".text = ''
+    [Desktop Entry]
+    Type=Application
+    Name=CopyQ
+    Exec=${pkgs.copyq}/bin/copyq
+    X-GNOME-Autostart-enabled=true
+  '';
+
+  # Super+V opens clipboard history
+  dconf.settings = {
+    "org/gnome/settings-daemon/plugins/media-keys" = {
+      custom-keybindings = [
+        "/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/copyq/"
+      ];
+    };
+    "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/copyq" = {
+      name = "CopyQ Clipboard History";
+      command = "${pkgs.copyq}/bin/copyq toggle";
+      binding = "<Super>v";
+    };
+  };
 }
