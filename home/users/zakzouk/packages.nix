01b1a46f70ea30434c912e04fa5198ca5c739f44
ZakzoukBT <zakaria.bentanfous@gmail.com>
2026-07-25 10:21:11 +0200

    feat: replace zed with vscode & add rmpc winboat podman and minecraft launcher

--- home/users/zakzouk/packages.nix
+++ home/users/zakzouk/packages.nix
@@ -4,8 +4,21 @@
   home.packages = with pkgs; [
     git
     brave
-    zed-editor
+    vscode
     steam
     wireshark
+    rmpc
+    winboat
+    podman
+    podman-compose
+    prismlauncher
+  ];
+
+  programs.vscode.extensions = [
+    "mhutchie.git-graph" #Git graph
+    "mathematic.vscode-latex" #LaTex
+    "james-yu.latex-workshop" #Latex Workshop 
+    "vscode-icons-team.vscode-icons" #vscode-icons
+    "jnoortheen.nix-ide" #Nix IDE
   ];
 }
