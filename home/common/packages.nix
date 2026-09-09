27cdebfb07038a5eba63f4f6cec94a565f590128
ZakzoukBT <zakaria.bentanfous@gmail.com>
2026-09-09 20:55:54 +0200

    refactor: remove CopyQ

--- home/common/packages.nix
+++ home/common/packages.nix
@@ -18,7 +18,6 @@
     btop
     nvtopPackages.amd
     wl-clipboard
-    copyq
     net-tools
     texliveFull
     texlab
@@ -39,27 +38,4 @@
     gcc
     gdb
   ];
-
-  # Autostart CopyQ on login
-  xdg.configFile."autostart/copyq.desktop".text = ''
-    [Desktop Entry]
-    Type=Application
-    Name=CopyQ
-    Exec=${pkgs.copyq}/bin/copyq
-    X-GNOME-Autostart-enabled=true
-  '';
-
-  # Super+V opens clipboard history
-  dconf.settings = {
-    "org/gnome/settings-daemon/plugins/media-keys" = {
-      custom-keybindings = [
-        "/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/copyq/"
-      ];
-    };
-    "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/copyq" = {
-      name = "CopyQ Clipboard History";
-      command = "${pkgs.copyq}/bin/copyq toggle";
-      binding = "<Super>v";
-    };
-  };
 }
